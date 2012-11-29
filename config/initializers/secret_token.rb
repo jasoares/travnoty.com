01ab4a88239dcc720f0f858edbace81ce121df2b
João Soares <jsoaresgeral@gmail.com>
2012-11-29 18:23:24 +0000

    Initial commit
    Rails project setup
    
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Travnoty::Application.config.secret_token = 'c5b470240c064566ccd65168722f4a93f79cfcbf7536f40c946ba7e0bceb7e63a431e17cd11c863e4f0adb9fb774325afacf6c68af2aecf313a205581c1d6e0e'

8db702bf929c176134d20ba015c87e9954630cfd
João Soares <jsoaresgeral@gmail.com>
2013-02-19 03:57:32 +0000

    Monkey patch Rake::Task to add #prerequisite_task!
    Rake::Task#prerequisite_task!, unlike the regular method
    following each prerequisite recursively.
    
    The patch was sent in a pull request to Jim Weirich.
    
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- features/step_definitions/task_steps.rb
+++ features/step_definitions/task_steps.rb
@@ -4,6 +4,19 @@ Rake.application.rake_require 'active_record/railties/databases'
 Rake.application.rake_require 'tasks/update'
 Rake::Task.define_task(:environment)
 
+class Rake::Task
+  # Recursively retrieve all task's prerequisite_tasks
+  def prerequisite_tasks!(limit=30, options={})
+    self.prerequisite_tasks.inject([]) do |tasks, task|
+      tasks << task
+      unless task.prerequisite_tasks.empty? or limit < 1
+        tasks += task.prerequisite_tasks!(limit-1)
+      end
+      tasks
+    end
+  end
+end
+
 Given /^I have a clean database$/ do
   step "I have no hubs"
   step "I have no servers"
@@ -11,10 +24,12 @@ Given /^I have a clean database$/ do
 end
 
 Given /^they are already loaded in the database$/ do
-  step "I run the task `rake db:seed`"
+  step "I run the task `rake travian:load`"
 end
 
-When /^I run the task `rake (\w+(?:\:\w+))`$/ do |task|
-  Rake::Task[task].reenable
-  Rake.application.invoke_task task
+When /^I run the task `rake (\w+(?:\:\w+)*)`$/ do |task|
+  task = Rake::Task[task]
+  task.reenable
+  task.prerequisite_tasks!.each(&:reenable)
+  task.invoke
 end

5b9aa8ebca7d3bb455dd30525a1c89a148a192e1
João Soares <jsoaresgeral@gmail.com>
2013-02-22 12:14:31 +0000

    Add specs for HubsController#index and #show
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- spec/controllers/api/v1/hubs_controller_spec.rb
+++ spec/controllers/api/v1/hubs_controller_spec.rb
@@ -6,4 +6,23 @@ describe Api::V1::HubsController do
 
     response.headers['X-Travnoty-Media-Type'].should == "application/vnd.travnoty.v1+json"
   end
+
+  context 'GET #index' do
+    it 'it renders the collection of all hubs as json' do
+      hubs = create_list(:hub, 3).to_json
+      get :index, format: :json
+
+      response.body.should == hubs
+    end
+  end
+
+  context 'GET #show' do
+    it 'renders the requested hub as json' do
+      hub = create(:hub, :with_servers, :with_mirrors)
+      json = hub.to_json
+      get :show, id: hub, format: :json
+
+      response.body.should == json
+    end
+  end
 end

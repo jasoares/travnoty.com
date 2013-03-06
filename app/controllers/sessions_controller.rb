ce09b5357666409397b6192e96696f37201397c7
João Soares <jsoaresgeral@gmail.com>
2013-03-06 18:54:43 +0000

    Improve application css styles
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       user.increment(:sign_in_count)
       user.update_attributes(last_sign_in_at: DateTime.now.utc)
       session[:user_id] = user.id
-      redirect_to root_url, :notice => 'Logged in!'
+      redirect_to root_url, :notice => 'Signed in!'
     else
       flash.now.alert = "Invalid email or password"
       render 'new'
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => 'Logged out!'
+    redirect_to root_url, :notice => 'Signed out!'
   end
 end

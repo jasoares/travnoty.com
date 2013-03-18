eacb2f3739073f5036ed1e99548514f2e047857e
João Soares <jsoaresgeral@gmail.com>
2013-03-18 19:26:51 +0000

    Change development smtp settings to sendgrid
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,11 +18,11 @@ Travnoty::Application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
+    :address              => "smtp.sendgrid.net",
     :port                 => 587,
-    :domain               => 'gmail.com',
-    :user_name            => ENV['GMAIL_USERNAME'],
-    :password             => ENV['GMAIL_PASSWORD'],
+    :domain               => 'travnoty.com',
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
 

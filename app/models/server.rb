5adf347d107edd69028cab3703313377533e5353
João Soares <jsoaresgeral@gmail.com>
2013-11-19 11:24:20 +0000

    Add Server::with_rounds scope to exclude servers
    without rounds from the results.
    
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- app/models/server.rb
+++ app/models/server.rb
@@ -8,25 +8,32 @@ class Server < ActiveRecord::Base
 
   scope :without_classics, where("host NOT ILIKE 'tc%'")
 
-  def self.ended
-    subquery = <<-SQL
-      SELECT r.server_id
-      FROM rounds AS r
-      GROUP BY r.server_id
-      HAVING every(r.end_date is not null)
-    SQL
+  class << self
+    def ended
+      subquery = <<-SQL
+        SELECT r.server_id
+        FROM rounds AS r
+        GROUP BY r.server_id
+        HAVING every(r.end_date is not null)
+      SQL
 
-    includes(:rounds).
-    where("rounds.id is null OR servers.id IN (#{subquery})")
-  end
+      includes(:rounds).
+      where("rounds.id is null OR servers.id IN (#{subquery})")
+    end
 
-  def self.restarting
-    joins(:rounds).where('rounds.start_date > ?', DateTime.now)
-  end
+    def restarting
+      joins(:rounds).where('rounds.start_date > ?', DateTime.now)
+    end
 
-  def self.running
-    joins(:rounds).
-    where('rounds.end_date is null and rounds.start_date < ?', DateTime.now)
+    def running
+      joins(:rounds).
+      where('rounds.end_date is null and rounds.start_date < ?', DateTime.now)
+    end
+
+    def with_rounds
+      joins("LEFT OUTER JOIN rounds ON servers.id = rounds.server_id").
+      where("rounds.server_id IS NOT NULL")
+    end
   end
 
   def current_round

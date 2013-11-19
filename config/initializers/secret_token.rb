330a0364a595959a71cd93e7f76b5b2268a8616a
João Soares <jsoaresgeral@gmail.com>
2013-11-19 12:43:43 +0000

    Add secret_key_base to initializers/secret_token.rb
    needed to migrato to Rails 4.
    
    Signed-off-by: João Soares <jsoaresgeral@gmail.com>

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Travnoty::Application.config.secret_token = 'c5b470240c064566ccd65168722f4a93f79cfcbf7536f40c946ba7e0bceb7e63a431e17cd11c863e4f0adb9fb774325afacf6c68af2aecf313a205581c1d6e0e'
+Travnoty::Application.config.secret_key_base = 'e8004ef7caa3eeeced460b167bcd3b044b86ceaf61efd5f79740800b984eb1c22f6a3d08769e1dc16ef59af487d99eef41ead6c0f00ee0f70b94f7f2974f6fe7'
